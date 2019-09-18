22ec6ac27ec8373fa7261adccfcdeabad2ef172b
frblondin <thomas@caudal.fr>
2019-09-18 08:41:03 +0200

    Convert projects to SDK format

--- src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
+++ src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2016-12-29 16:26:02
+// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2019-09-17 17:56:27
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
